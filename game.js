a48f21b118033e0e9ab7b9cd9f844554752339bc
Frede <freddyzorio12@gmail.com>
2025-04-14 23:02:37 -0600

    second commit

--- game.js
+++ game.js
@@ -2,11 +2,26 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  var audio = new Audio("sounds/" + userChosenColour + ".mp3");
+  audio.play();
+  userClickedPattern.push(userChosenColour);
+});
+
+function animatePress(currentColour) {
+  $(".btn").addClass("pressed");
+}
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
 }
